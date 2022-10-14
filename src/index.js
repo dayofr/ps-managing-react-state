488ca51d38258e0847d3019ca0c0af1068a16e67
apavel <aurelien.pavel@neotys.com>
2022-10-14 17:27:04 +0200

    Move to react 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 import ErrorBoundary from "./ErrorBoundary";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <ErrorBoundary>
     <App />
-  </ErrorBoundary>,
-  document.getElementById("root")
+  </ErrorBoundary>
 );
